030a141f65a297c876ca2802ec0fd060d0b8058f
Ali Hoseini <a.h.computer73@gmail.com>
2024-05-27 09:07:09 +0330

    completing app catalogue

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from catalogue.views import products_list
+from catalogue.views import products_list, product_detail, category_products
 
 urlpatterns = [
-    path("products/list/", products_list, name="products_list")
+    path("products/list/", products_list, name="products_list"),
+    path("product/detail/<int:pk>/", product_detail, name="product_detail"),
+    path("category/<int:pk>/products/", category_products, name="category_products"),
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,5 +1,26 @@
+from django.db.models import Q
 from django.http import HttpResponse
+
+from catalogue.models import Product
 
 
 def products_list(request):
-    return HttpResponse("products list page")
+    products = Product.objects.select_related("category").all()
+    context = [
+        (f"{product.title}--{product.upc}--{product.product_type}"
+         f"--{product.category.title}--{product.brand}--{product.is_active} <br>") for product in products
+    ]
+    return HttpResponse(context)
+
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(Q(pk=pk) | Q(upc=pk))
+    except Product.DoesNotExist:
+        return HttpResponse("this product does not exist")
+
+    return HttpResponse(product)
+
+
+def category_products(request, pk):
+    pass
