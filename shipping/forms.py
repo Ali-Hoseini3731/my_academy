e6873ef186d85908ca97239206bcc8428805317a
Ali Hoseini <a.h.computer73@gmail.com>
2024-07-17 07:19:29 +0330

    use class based view

--- shipping/forms.py
+++ shipping/forms.py
@@ -1,12 +1,27 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lib.validation import len_zipcode
 from shipping.models import ShippingAddress
 
 
 class ShippingAddressForm(forms.ModelForm):
-    zipcode = forms.CharField(validators=[len_zipcode])
+    # zipcode = forms.CharField(validators=[len_zipcode])
 
     class Meta:
         model = ShippingAddress
-        fields = ("city", "zipcode", "address", "number")
+        fields = ["city", "zipcode", "address", "number"]
+
+    def clean_zipcode(self):
+        zipcode = self.cleaned_data["zipcode"]
+        if len(zipcode) <= 2:
+            raise ValidationError("The Length must be greater than 2 !")
+
+        return zipcode
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     zipcode = cleaned_data["zipcode"]
+    #     if len(zipcode) <= 2:
+    #         raise ValidationError("The Length must be greater than 2 !")
+    #     return cleaned_data
